3819e2976936cd053d81b6600ccc3dcdd6b1f144
bitzo <bitzo@qq.com>
2017-07-08 19:15:59 +0800

    update: 修改README中的错字

--- app.js
+++ app.js
@@ -24,9 +24,7 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.all('/api/*', apiAuth);
-
 app.use('/', routes);
-// app.use('/users', users);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
