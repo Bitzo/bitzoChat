27b094fd1b8ceb9be9cc4c5038991053e325c05d
bitzo <bitzo@qq.com>
2017-12-03 19:36:45 +0800

    update: 修改配置

--- config/config.js
+++ config/config.js
@@ -1,6 +1,12 @@
 const config = {
-  // mtsql config
+  // mysql config
   mysql: {
+    host: '',
+    user: '',
+    password: '',
+  },
+  // redisConfig
+  redis: {
     host: '',
     user: '',
     password: '',
